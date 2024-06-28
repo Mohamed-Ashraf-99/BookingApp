3d0b84d19fe7ef11e8cb0e3e589c45a579037be0
Mohamed-Ashraf-99 <mohamedashrafita@gmail.com>
2024-06-28 22:17:29 +0300

    Edit Complains Handler

--- Booking.Application/UserComplains/Commands/Create/CreateComplainCommandHandler.cs
+++ Booking.Application/UserComplains/Commands/Create/CreateComplainCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using AutoMapper;
+using Booking.Application.Services.CurrentUser;
 using Booking.Domain.Entities;
 using Booking.Domain.Repositories;
 using MediatR;
@@ -10,12 +11,17 @@ namespace Booking.Application.UserComplains.Commands.Create;
 
 public class CreateComplainCommandHandler(ILogger<CreateComplainCommandHandler> _logger,
     IComplainsRepository _complainsRepository,
-    IMapper _mapper) : IRequestHandler<CreateComplainCommand, string>
+    IMapper _mapper,
+    ICurrentUserService _currentUserService,
+    IClientRepository _clientRepository) : IRequestHandler<CreateComplainCommand, string>
 {
     public async Task<string> Handle(CreateComplainCommand request, CancellationToken cancellationToken)
     {
         _logger.LogInformation($"Client with id {request.ClientId} Make a completion");
         var complain = _mapper.Map<Complains>(request);
+        var userId = await _currentUserService.GetUserId();
+        var clientId = await _clientRepository.GetClientIdByUserId(userId);
+        complain.ClientId = clientId;
         var response = await _complainsRepository.CreateAsync(complain);
         if (response > 0)
             return "Succeeded";
