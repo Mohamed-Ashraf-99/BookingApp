3eb8c6be745e5783ebf2cd55945879403ee56dc4
Mohamed-Ashraf-99 <mohamedashrafita@gmail.com>
2024-06-30 20:40:23 +0300

    Edit owner Repo

--- Booking.Domain/Repositories/IOwnerRepository.cs
+++ Booking.Domain/Repositories/IOwnerRepository.cs
@@ -1,25 +1,23 @@
-﻿
-
-using Booking.Domain.Entities;
+﻿using Booking.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Booking.Domain.Repositories;
+//namespace Booking.Domain.Repositories;
 
 namespace Booking.Domain.Repositories
 {
     public interface IOwnerRepository
     {
-    Task<int> CreateAsync(Owner owner);
-    Task<Owner> GetOwnerByUserId(int Id);
-    Task<IEnumerable<Owner>> GetAllUnVerifiedOwners();
-    Task<Owner> GetOwnerById(int id);
-    Task<bool> UpdateOwnerAsync(Owner owner);
-    Task<int> GetUserIdByOwnerId(int ownerId);
-    Task<bool> DeleteAsync(int ownerId);
+        Task<int> CreateAsync(Owner owner);
+        Task<Owner> GetOwnerByUserId(int Id);
+        Task<IEnumerable<Owner>> GetAllUnVerifiedOwners();
+        Task<Owner> GetOwnerById(int id);
+        Task<bool> UpdateOwnerAsync(Owner owner);
+        Task<int> GetUserIdByOwnerId(int ownerId);
+        Task<bool> DeleteAsync(int ownerId);
         Task<int> GetOwnerIdByUserId(int userId);
 
         Task<int> AddHotels(Hotel hotel);
